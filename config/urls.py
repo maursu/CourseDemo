55d8d8e4120d65320f19a3aa7e2a89438631ca34
TimaDootaliev <timur.dootaliev.td@gmail.com>
2023-06-22 16:05:31 +0600

    setup #4

--- config/urls.py
+++ config/urls.py
@@ -36,5 +36,5 @@ urlpatterns = [
 if settings.DEBUG:
     from django.conf.urls.static import static
 
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # noqa
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

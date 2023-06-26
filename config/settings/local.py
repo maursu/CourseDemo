33fbe7b571ea94ae62f8ea8d9c522128aec8cf0d
TimaDootaliev <timur.dootaliev.td@gmail.com>
2023-06-26 16:46:24 +0600

    settings changed #6

---
+++ config/settings/local.py
@@ -0,0 +1,17 @@
+from .settings import *
+
+import os
+
+
+DEBUG = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": os.getenv("DB_ENGINE"),
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
+    }
+}
---
+++ config/settings/prod.py
@@ -0,0 +1,21 @@
+from .settings import *
+import os
+
+
+DEBUG = False
+
+ADMINS = (("admin", "admin@admin.com"),)
+
+ALLOWED_HOSTS = ["*"]
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": os.getenv("DB_ENGINE"),
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
+    }
+}
